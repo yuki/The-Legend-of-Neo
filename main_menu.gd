50fcb9a5b5f812cf5c5f9fb28a476c558aeee0fc
Yuki (aka Rubén Gómez) <rugoli@gmail.com>
2024-08-31 10:54:12 +0200

    [Menu] Added buttons
    And first interaction.

---
+++ main_menu.gd
@@ -0,0 +1,26 @@
+extends Control
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print(TranslationServer.get_locale())
+	#pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_packed(load("res://scenes/recorded/intro.tscn"))
+	#pass # Replace with function body.
+
+
+func _on_options_pressed() -> void:
+	$IntroContainer.visible = !$IntroContainer.visible
+	$OptionsContainer.visible = !$OptionsContainer.visible
+
+
+func _on_back_pressed() -> void:
+	$IntroContainer.visible = !$IntroContainer.visible
+	$OptionsContainer.visible = !$OptionsContainer.visible
